f9a301e1a713ed2828f86649de8bd721a30f67b3
Ernest Przybył <ernestprzybyl@gmail.com>
2023-01-21 18:57:42 +0100

    add start script, add comment

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,6 +7,7 @@ type Props = {};
 export const MainPage = (props: Props) => {
   return (
     <div className="max-w-3xl mx-auto p-3 min-h-screen flex flex-col items-center justify-center">
+      {/* Using context is overkill. But I used it just to show. */}
       <ModalContextProvider>
         <ProductsTable className="w-full"/>
       </ModalContextProvider>
--- src/repositories/IProductRepository.ts
+++ src/repositories/IProductRepository.ts
@@ -1,6 +1,8 @@
 import { Pagination } from "../model/Pagination";
 import { Product } from "../model/Product";
 
+// this pattern makes us independent of fetch api, but 
+// it makes hard to abort request
 export interface IProductRepository {
   getProductById(id: number): Promise<Product>;
   getProductsPagination(page: number, per_page: number): Promise<Pagination<Product>>;
